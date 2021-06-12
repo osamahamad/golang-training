cf1a241f5765b076a6622c6127cf4b916d0fad54
osamahamad <asamamaxsteam@gmail.com>
2021-06-12 14:55:36 +0200

    2

--- Variables/test.go
+++ Variables/test.go
@@ -4,6 +4,18 @@ import (
 
 	"fmt"
 )
+
+
+// We can declare/initilize variables outside the main function ..
+
+
+var (
+
+actor = "john mark"
+age = 32
+
+
+)
 func main() {
 // Variables in go can vary 
 
@@ -26,15 +38,33 @@ fmt.Println(x)
 
 
 // Another way to initilize variables 
-// short variable declaration
+// short variable declaration assignment :=
 z := 25
-fmt.Println(z)
+fmt.Println(z) 
 fmt.Printf("Variable value %v and have type of %T \n", z, z)
 
 
 xx := int64(1)
 aa := "wtf"
 fmt.Printf("Variable value %v and have type of %T, another one %v \n", xx, xx, aa)
+fmt.Printf("Variable value %v and have type of %T, another one %v \n", actor , actor, age)
+
+// You can't declare the variable twice in the same scope .
+aa = "no fak"
+fmt.Println(aa)
+
+// You will get comple time error if you variable declared and not used in go 
+
+
+// convert from one variabe type to another with convertion function
+
+
+var newvariable11 int = 42 
+fmt.Printf("%v , %T",newvariable11,newvariable11)
+
+var j float32 = float32(i)
+fmt.Printf("%v , %T\n",j,j)
+fmt.Printf("%v ,%T, %T\n",i,i,float32(i))
 
 
 }
